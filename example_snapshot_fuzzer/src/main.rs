cd362c272c1b34f5e1df74190c707ccdbd11d3f2
Brandon Falk <bfalk@gamozolabs.com>
2022-02-23 04:20:16 -0800

    1 core by default for plebs

--- example_snapshot_fuzzer/src/main.rs
+++ example_snapshot_fuzzer/src/main.rs
@@ -239,7 +239,7 @@ fn main() -> Result<()> {
 
     // Fork the VM for each thread
     std::thread::scope(|s| {
-        for _ in 0..32 {
+        for _ in 0..1 {
             let vm = orig_vm.clone();
             s.spawn(|_| {
                 worker(&orig_vm, vm, &stats, fuzz_input, fuzz_input_len);
